7f5ecab148a810c95e96bd5bde4b8395bcd13834
Francois Polo <andasan@gmail.com>
2022-04-12 10:03:25 -0700

    added a sidebar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
 import Comments from "./pages/Comments";
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 import PostPage from "./pages/PostPage";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 // reddit/r/search?q=cats&src=caturday
 // {q: 'cats',src: 'caturday'}
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard name={'Hoge'} />} />
-        <Route path="/post/:postId" element={<PostPage />} />
-        <Route path="/comments" element={<Comments />} />
-        <Route path="/profile" element={<Profile />} />
+    <div className="wrapper">
 
-        <Route path="*" element={<h4>WHOOPS~!!! 404 Page Not Found!</h4>} />
-      </Routes>
-    </>
+      <div className="siderbar">
+        <ul className="nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+        </ul>
+        {/* <Routes>
+          <Route path="/" element={<p>This is your home page</p>} />
+          <Route path="/login" element={<p>This is your login page</p>} />
+          <Route path="/register" element={<p>This is your register page</p>} />
+        </Routes> */}
+      </div>
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard name={"Hoge"} />} />
+          <Route path="/post/:postId" element={<PostPage />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="*" element={<h4>WHOOPS~!!! 404 Page Not Found!</h4>} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = ({ name }) => {
   return (
     <>
         <h1>Dashboard</h1>
         <p>Welcome, {name}</p>
+        <hr />
+        <Link to="/post/1">Link to Post ID 1</Link>
     </>
   )
 }
