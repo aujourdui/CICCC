02b5559d19b6c052c89c45f7e491078bedec344a
Francois Polo <andasan@gmail.com>
2022-04-12 11:04:14 -0700

    added route config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, useRoutes } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Comments from "./pages/Comments";
-import Dashboard from "./pages/Dashboard";
-import PostPage from "./pages/PostPage";
-import Profile from "./pages/Profile";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import CustomLink from "./components/CustomLink";
+import Chats from "./pages/Chats";
+import Messages from "./pages/Messages";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Comments = React.lazy(() => import("./pages/Comments"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const PostPage = React.lazy(() => import("./pages/PostPage"));
+const Profile = React.lazy(() => import("./pages/Profile"));
+const Register = React.lazy(() => import("./pages/Register"));
+const Login = React.lazy(() => import("./pages/Login"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 // reddit/r/search?q=cats&src=caturday
 // {q: 'cats',src: 'caturday'}
 
 const App = () => {
+  const [user, setUser] = useState('')
+  
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    { path: "/post/:postId", element: <PostPage /> },
+    { path: "/comments", element: <Comments /> },
+    { path: "/profile", element: <Profile /> },
+    { path: "*", element: <NotFound /> },
+    { 
+      path: "/messages", 
+      element: <Messages />, 
+      children: [
+        { path: ":id", element: <Chats /> }
+      ]
+    },
+  ])
   return (
     <div className="wrapper">
       <div className="sidebar">
         <ul className="nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
+          <li>
+            <CustomLink to="/">Home</CustomLink>
+          </li>
+          <li>
+            <CustomLink to="/login">Login</CustomLink>
+          </li>
+          <li>
+            <CustomLink to="/register">Register</CustomLink>
+          </li>
+          <li>
+            <CustomLink to="/dashboard">Dashboard</CustomLink>
+          </li>
         </ul>
         {/* <Routes>
           <Route path="/" element={<p>This is your home page</p>} />
@@ -29,17 +62,22 @@ const App = () => {
       </div>
 
       <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard name={"Hoge"} />} />
-          <Route path="/post/:postId" element={<PostPage />} />
-          <Route path="/comments" element={<Comments />} />
-          <Route path="/profile" element={<Profile />} />
-
-          <Route path="*" element={<h4>WHOOPS~!!! 404 Page Not Found!</h4>} />
-        </Routes>
+        <React.Suspense fallback={<p>Loading...</p>}>
+          {routes}
+          {/* <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard name={"Hoge"} />} />
+            <Route path="/post/:postId" element={<PostPage />} />
+            <Route path="/comments" element={<Comments />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messages" element={<Messages />}>
+              <Route path=":id" element={<Chats />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes> */}
+        </React.Suspense>
       </div>
     </div>
   );
