4236a60ef4db23eaa3eb9c2dbc0bb4eb69e718c5
Francois Polo <andasan@gmail.com>
2022-04-12 09:53:01 -0700

    rrdpt2.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Comments from "./pages/Comments";
+import Dashboard from './pages/Dashboard';
+import PostPage from "./pages/PostPage";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+
+// reddit/r/search?q=cats&src=caturday
+// {q: 'cats',src: 'caturday'}
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard name={'Hoge'} />} />
+        <Route path="/post/:postId" element={<PostPage />} />
+        <Route path="/comments" element={<Comments />} />
+        <Route path="/profile" element={<Profile />} />
+
         <Route path="*" element={<h4>WHOOPS~!!! 404 Page Not Found!</h4>} />
       </Routes>
     </>
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = ({ name }) => {
+  return (
+    <>
+        <h1>Dashboard</h1>
+        <p>Welcome, {name}</p>
+    </>
+  )
+}
+
+export default Dashboard
