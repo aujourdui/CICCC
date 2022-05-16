6fd43926a47ec2df0baa0899941536c5d504a0c3
ito <kyosuke.ito@gmail.com>
2022-05-16 12:10:48 -0700

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,47 @@
-const uuid = require('uuid')
-const router = require('express').Router()
-const {client} = require('../services/redis')
+const uuid = require("uuid");
+const router = require("express").Router();
+const { client } = require("../services/redis");
 
-router.get('/', async (req, res) => {
-    try{
-        const todos = []
-        // const todos = await client.hGetAll('todos')
-        // console.log(todos);
+router.get("/", async (req, res) => {
+  try {
+    const todos = [];
+    // const todos = await client.hGetAll('todos')
+    // console.log(todos);
 
-        for await (const { field, value } of client.hScanIterator("todos") ){
-            todos.push({ ID: field, Title: value })
-        }
-
-        res.render('index', { model: todos })
-    }catch(e){
-        console.error(e)
+    for await (const { field, value } of client.hScanIterator("todos")) {
+      todos.push({ ID: field, Title: value });
     }
-})
 
-router.get('/create', (req, res) => {
-    res.render('create', { model: {} })
-})
+    res.render("index", { model: todos });
+  } catch (e) {
+    console.error(e);
+  }
+});
 
-router.post('/create', async (req, res) => {
-    await client.hSet("todos", uuid.v4(), req.body.Title)
-    res.redirect('/')
-})
+router.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
 
-module.exports = router
+router.post("/create", async (req, res) => {
+  await client.hSet("todos", uuid.v4(), req.body.Title);
+  res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const result = await client.hGet("todos", req.params.id);
+  // console.log(result);
+  res.render("edit", { model: {} });
+});
+
+router.post("/edit", async (req, res) => {
+  await client.hSet("todos", req.params.id, req.body.Title);
+  console.log(req.params.id);
+  res.redirect("/");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  await client.hDel("todos", req.params.id);
+  res.redirect("/");
+});
+
+module.exports = router;
